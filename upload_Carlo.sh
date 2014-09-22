a9471c1b7744979ec6be9e5b45dd506e2598675a
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-09-22 15:00:20 +0200

    Sample upload scripts.

---
+++ upload_Carlo.sh
@@ -0,0 +1,3 @@
+mkdir /tmp/processed_carlo
+.cabal-sandbox/bin/imagetrove-uploader --input-dir /tmp/MP2RAGE-WIP900_1_0ISO_7T_T1_IMAGES_0003 --config Carlo.conf --processed-dir /tmp/processed_carlo upload-all 
+
---
+++ upload_SAHMRI.sh
@@ -0,0 +1,3 @@
+mkdir /tmp/processed_SAHMRI
+.cabal-sandbox/bin/imagetrove-uploader --input-dir /tmp/Dicom_SAHMRI/ --config SAHMRI.conf --processed-dir /tmp/processed_SAHMRI upload-all 
+
