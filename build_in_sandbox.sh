823ae5761618a2bfadd58ac1caa1444411e27409
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2015-02-26 15:29:56 +1000

    Specific Cabal version?

--- build_in_sandbox.sh
+++ build_in_sandbox.sh
@@ -8,6 +8,6 @@ rm -fr .cabal-sandbox cabal.sandbox.config dist
 
 cabal sandbox init
 
-cabal install Cabal
+cabal install Cabal-1.20.0.2
 cabal install --haddock-hyperlink-source --dependencies-only --force-reinstalls
 cabal install --haddock-hyperlink-source
