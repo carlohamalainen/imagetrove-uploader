e23e7e3b42f473a1c9422535a1b04903c9e7ccf8
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2015-03-02 13:31:13 +1000

    Convenience wrapper for get/set state.

--- src/Network/ImageTrove/Acid.hs
+++ src/Network/ImageTrove/Acid.hs
@@ -2,15 +2,17 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
-module Network.ImageTrove.Acid where
+module Network.ImageTrove.Acid (getLastRunTime, setLastRunTime) where
 
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.Acid
 import Data.SafeCopy
 import Data.Time.Clock (getCurrentTime, diffUTCTime, secondsToDiffTime, UTCTime(..))
+import Data.Time.Format (parseTime)
 import Data.Typeable
 import System.Environment
+import System.Locale (defaultTimeLocale)
 
 data LastRunState = LastRunState UTCTime deriving (Show, Typeable)
 
@@ -25,15 +27,15 @@ queryState = do LastRunState t <- ask
 
 $(makeAcidic ''LastRunState ['writeState, 'queryState])
 
-{-
-mmain :: IO ()
-mmain = do now <- getCurrentTime
-           acid <- openLocalState $ LastRunState now
-           args <- getArgs
-           if null args
-           then do string <- query acid QueryState
-                   putStrLn $ "The state is: " ++ show string
-           else do -- update acid (WriteState (unwords args))
-                   -- putStrLn "The state has been modified!"
-                   putStrLn "derp"
--}
+time1970 :: UTCTime
+Just time1970 = parseTime defaultTimeLocale "%Y%m%dT%H%M%S" "19700101000000"
+
+getLastRunTime :: IO UTCTime
+getLastRunTime = do
+    acid <- openLocalState $ LastRunState time1970
+    query acid QueryState
+
+setLastRunTime :: UTCTime -> IO ()
+setLastRunTime t = do
+    acid <- openLocalState $ LastRunState time1970
+    update acid $ WriteState t
